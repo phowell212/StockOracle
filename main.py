99e3aac01147f26d5f8f43fc1c610b4f2717848b
phowell212 <phowell2@stevens.edu>
2025-04-10 00:42:59 -0400

    Got rid of bad stock-data getting

--- main.py
+++ main.py
@@ -17,35 +17,7 @@ app.layout = html.Div([
     dcc.Input(id='ticker', type='text', placeholder='Enter Ticker Symbol', style={'marginRight': '10px'}),
     html.Button('Update', id='update-button', n_clicks=0, style={'marginTop': '10px'}),
 
-    # graph (output)
-    html.Div(id='live-graph', style={'width': '100%', 'height': '100vh'}),
-    dcc.Graph(id='graph', style={'width': '100%', 'height': '100vh'}),
-
 ])
-
-
-@app.callback(
-    Output('graph', 'figure'),
-    Input('update-button', 'n_clicks'),
-    State('ticker', 'value')
-)
-def update_graph(n_clicks, ticker):
-    if n_clicks > 0 and ticker:
-        # Fetch data from Yahoo Finance
-        data = yfinance.download(ticker, period='1d', interval='1m')
-        fig = {
-            'data': [
-                {'x': data.index, 'y': data['Close'], 'type': 'line', 'name': ticker}
-            ],
-            'layout': {
-                'title': f'Live Stock Data for {ticker}',
-                'xaxis': {'title': 'Time'},
-                'yaxis': {'title': 'Price'},
-            }
-        }
-        return fig
-    else:
-        return {'data': [], 'layout': {}}
 
 
 if __name__ == '__main__':
